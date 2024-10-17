ae6f2741b92770e4dac84cb65e23928323b8ff4b
foxen328 <155308416+foxen328@users.noreply.github.com>
2024-10-17 15:58:32 +0100

    v1.0
    Got manual input working as expected

--- index.js
+++ index.js
@@ -3,10 +3,16 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
-const insertedValue = 20;
-const inputNumber = document.getElementById("input-field");
+//let inputNumber = 20;
+let inputNumber = document.getElementById("input-field").value;
+//inputNumber = Number(inputNumber);
 const convertBtn = document.getElementById("convert-btn");
-const test = document.getElementById("return-value-three")
+const lengthInsert = document.getElementById("length");
+const volumeInsert = document.getElementById("volume");
+const massInsert = document.getElementById("mass");
+
+//inputNumber = inputNumber - Testing reassignment to get the inputted value to function and allow a value to be passed into functions
+//console.log(inputNumber)
 
 const feet = 3.281;
 const meter = 1;
@@ -15,42 +21,55 @@ const gallon = 0.264;
 const kilogram = 1;
 const pound = 2.204;
 
+//Dark mode toggle variable
+let darkMode = document.getElementById("dark-mode");
+
 // convertBtn.addEventListener("click", function () {
 
-// })
 
-// function feetToMeters() {
+convertBtn.addEventListener("click", function () {
 
-// }
-
-// function metersToFeet() {
-
-// }
-
-// function litersToGallon() {
-
-// }
-
-// function gallonsToLiters() {
-
-// }
-
-// function kilogramToPound() {
-
-// }
-
-function poundToKilogram() {
-  let result = insertedValue * pound
-  return result.toFixed(3)
-}
-convertBtn.addEventListener("click", function{
-
+  length()
+  volume()
+  mass()
 
 })
+
+function length() {
+  let inputNumber = document.getElementById("input-field").value;
+  let metersToFeet = inputNumber * feet
+  let feetToMeters = inputNumber / feet
+  let contents = `${inputNumber} meters = ${metersToFeet.toFixed(3)} feet | ${inputNumber} feet = ${feetToMeters.toFixed(3)} meters`
+  lengthInsert.innerHTML = contents
+}
+
+function volume() {
+  let inputNumber = document.getElementById("input-field").value;
+  let kilosToPounds = inputNumber * pound
+  let poundsToKilos = inputNumber / pound
+  let contents = `${inputNumber} kilos = ${kilosToPounds.toFixed(3)} pounds | ${inputNumber} pounds = ${poundsToKilos.toFixed(3)} kilos`
+  volumeInsert.innerHTML = contents
+}
+
+function mass() {
+  let inputNumber = document.getElementById("input-field").value;
+  let kilosToPounds = inputNumber * pound
+  let poundsToKilos = inputNumber / pound
+  let contents = `${inputNumber} kilos = ${kilosToPounds.toFixed(3)} pounds | ${inputNumber} pounds = ${poundsToKilos.toFixed(3)} kilos`
+  massInsert.innerHTML = contents
+}
 
 
 //Flow chat of what I want to do
 // So essentially, I want to store the value from the input field (done)
 
 //From there, we want to create conversions below first before we doing anything else
+
+
+// darkMode.addEventListener("click", function () {
+
+//   let dark = document.body;
+//   dark.classList.toggle("dark-mode")
+
+// })
 
